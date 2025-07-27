ffb4ffa75e6e73c8a83451ca47c855cca403182f
bedirhan-1 <bedirhan9828@gmail.com>
2025-07-27 18:46:42 +0300

    pazar günü

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ export default async function Home() {
   try {
     let response = await fetch("https://jsonplaceholder.typicode.com/users");
     var data = await response.json();
-  } catch {}
+  } catch (error) {
+    console.log(error);
+  }
 
   return (
-    <div className="flex flex-col container h-screen mx-auto">
+    <div className="flex flex-col container h-screen mx-auto space-y-4 mt-8">
       {data.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
--- components/UserCard.js
+++ components/UserCard.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export const UserCard = ({ user }) => {
-  return <h1>{user.name}</h1>;
+  return (
+    <Link href={`/${user.id}`} className="bg-pink-200 rounded-lg px-6 py-2">
+      <h1 className="text-2xl italic font-bold">{user.username}</h1>
+      <div className="flex gap-8">
+        <h3>{user.email}</h3>
+        <h3>{user.name}</h3>
+      </div>
+    </Link>
+  );
 };
